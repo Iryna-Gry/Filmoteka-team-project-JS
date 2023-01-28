c0004870f20ab30a9fa15a4698572b9166fe5ac7
Iryna Grytsaenko <iryna.grytsaenko@gmail.com>
2023-01-28 14:10:29 +0200

    Update firebase-config.js
    changes firebase config

--- src/js/api/firebase-config.js
+++ src/js/api/firebase-config.js
@@ -4,13 +4,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: 'AIzaSyDv5dHMjgq_6GWD35Of-cTXskRk1rXncws',
-  authDomain: 'filmoteka-command-project-js.firebaseapp.com',
-  projectId: 'filmoteka-command-project-js',
-  storageBucket: 'filmoteka-command-project-js.appspot.com',
-  messagingSenderId: '1084933608284',
-  appId: '1:1084933608284:web:16097211c819e6fa8d6799',
-  measurementId: 'G-BHYJP9WVT4',
+  apiKey: 'AIzaSyCxe7WeI3T2Iw-1xMaPGWEO4zfuSfDe7gY',
+  authDomain: 'filmoteka-team-project-j-9925d.firebaseapp.com',
+  projectId: 'filmoteka-team-project-j-9925d',
+  storageBucket: 'filmoteka-team-project-j-9925d.appspot.com',
+  messagingSenderId: '410025843843',
+  appId: '1:410025843843:web:481e052239d4c2f1930fb6',
+  measurementId: 'G-WJ2KQ7LF58',
 };
 
 // Initialize Firebase
